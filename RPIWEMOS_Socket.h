f97379b6f4e64ad4f4df364dfbe87e3815573aa6
Lucas Jansze <lucas.jansze@gmail.com>
2026-05-12 10:47:48 +0200

    Add SocketCommunicatie class for socket communication

---
+++ RPIWEMOS_Socket.h
@@ -0,0 +1,29 @@
+#ifndef RPIWEMOS_SOCKET_H
+#define RPIWEMOS_SOCKET_H
+
+#include <string>
+#include <chrono>
+#include <mutex>
+
+class SocketCommunicatie {
+private:
+    std::string ipAdresDoel;
+    int poort;
+    bool isVerbonden;
+
+    int server_fd;
+    std::string laatsteData;
+    std::mutex data_mutex;
+    std::chrono::time_point<std::chrono::steady_clock> laatsteOntvangstTijd;
+
+public:
+    SocketCommunicatie(std::string ipAdresDoel, int poort);
+    ~SocketCommunicatie();
+
+    bool verbind();
+    void verzendData(std::string bericht);
+    std::string ontvangData();
+    bool checkConnectieStatus();
+};
+
+#endif
