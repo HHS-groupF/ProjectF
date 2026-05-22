53decf1db740b0ec69de19ae8adab7a83206a366
Pouriyamehr <Pouriyamehr@users.noreply.github.com>
2026-05-22 10:38:28 +0000

    deploy: 214d9be6a373075fb8790ee19facca30f3f8bae0

--- search/all_7.js
+++ search/all_7.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['main_0',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;main.cpp'],['../WemosDashboard_2main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;main.cpp']]],
+  ['main_0',['main',['../Socketverbinding_2main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;main.cpp'],['../WemosDashboard_2main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main():&#160;main.cpp']]],
   ['main_2ec_1',['main.c',['../main_8c.html',1,'']]],
-  ['main_2ecpp_2',['main.cpp',['../main_8cpp.html',1,'(Global Namespace)'],['../WemosDashboard_2main_8cpp.html',1,'(Global Namespace)']]],
+  ['main_2ecpp_2',['main.cpp',['../Socketverbinding_2main_8cpp.html',1,'(Global Namespace)'],['../WemosDashboard_2main_8cpp.html',1,'(Global Namespace)']]],
   ['mqtt_5fclient_5fid_3',['MQTT_CLIENT_ID',['../WemosDashboard_2config_8h.html#a87467ea560dbba1fe10d066adb9f4a11',1,'config.h']]],
   ['mqtt_5fport_4',['MQTT_PORT',['../WemosDashboard_2config_8h.html#aa8632baff6bbb5004385998918f1e6bd',1,'config.h']]],
   ['mqtt_5fserver_5',['mqtt_server',['../Wemos-tafel_2config_8h.html#a977bd2592e99309a86e7034189e9e62d',1,'MQTT_SERVER:&#160;config.h'],['../WemosDashboard_2config_8h.html#a977bd2592e99309a86e7034189e9e62d',1,'MQTT_SERVER:&#160;config.h']]],
