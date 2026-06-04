b2e3ca79e56c23dc7b2b4f35d878a9a33b79ffc7
Lucas Jansze <lucas.jansze@gmail.com>
2026-06-04 16:28:32 +0200

    Integrate CAN bus communication in main.cpp
    Added CAN bus communication functionality to the main application.

--- RPI-BUS/main.cpp
+++ RPI-BUS/main.cpp
@@ -1,6 +1,7 @@
 #include <QCoreApplication>
 #include "SocketCommunicatieRPIBUS.h"
 #include "CentraalBesturingssysteemRPIBUS.h"
+#include "CanBusCommunicatieRPIBUS.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +10,7 @@ int main(int argc, char *argv[])
     // Maak de netwerkmodule en de logica module aan
     SocketCommunicatieRPIBUS socket;
     CentraalBesturingssysteemRPIBUS systeem;
+    CanBusCommunicatieRPIBUS canBus;
 
     // 1. Koppel inkomende netwerkcommando's (vanaf WEMOS) aan het systeem
     QObject::connect(&socket, &SocketCommunicatieRPIBUS::inkomendCommando,
@@ -17,8 +19,17 @@ int main(int argc, char *argv[])
     // 2. Koppel uitgaande data en statussen (vanuit het systeem) aan het netwerk
     QObject::connect(&systeem, &CentraalBesturingssysteemRPIBUS::stuurNetwerkData,
                      &socket, &SocketCommunicatieRPIBUS::verzendData);
+    
+    // NIEUW: CAN -> Systeem (Binnenkomende STM32 Sensor Data)
+    QObject::connect(&canBus, &CanBusCommunicatieRPIBUS::inkomendeSensorData,
+                     &systeem, &CentraalBesturingssysteemRPIBUS::verwerkCanSensorData);
 
+    // NIEUW: Systeem -> CAN (Uitgaande STM32 Commando's)
+    QObject::connect(&systeem, &CentraalBesturingssysteemRPIBUS::stuurCanCommando,
+                     &canBus, &CanBusCommunicatieRPIBUS::verstuurCommandoNaarSTM);
     // Start de servers/clients
     socket.start();
+    canBus.start("can0");
+    
     return a.exec();
 }
