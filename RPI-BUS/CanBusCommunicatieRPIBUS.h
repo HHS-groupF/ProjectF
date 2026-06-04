59bfb66ab38bc2d71189db66cbbab8e0ac497ec9
Lucas Jansze <lucas.jansze@gmail.com>
2026-06-04 16:18:30 +0200

    Add files via upload

---
+++ RPI-BUS/CanBusCommunicatieRPIBUS.h
@@ -0,0 +1,32 @@
+#ifndef CANBUSCOMMUNICATIERPIBUS_H
+#define CANBUSCOMMUNICATIERPIBUS_H
+
+#include <QObject>
+#include <QCanBus>
+#include <QCanBusDevice>
+#include <QCanBusFrame>
+#include <QDebug>
+
+class CanBusCommunicatieRPIBUS : public QObject {
+    Q_OBJECT
+public:
+    explicit CanBusCommunicatieRPIBUS(QObject *parent = nullptr);
+    ~CanBusCommunicatieRPIBUS();
+    void start(const QString &interfaceName = "can0");
+
+public slots:
+    // Slot om commando's (bijv. ventilator of LED aan/uit) naar een specifieke STM32 te sturen
+    void verstuurCommandoNaarSTM(uint32_t stmId, const QByteArray &data);
+
+signals:
+    // Signaal dat wordt afgevuurd als we data ontvangen van de STM32
+    void inkomendeSensorData(uint32_t stmId, const QString &sensorType, double waarde);
+
+private slots:
+    void ontvangCanBerichten();
+
+private:
+    QCanBusDevice *canDevice = nullptr;
+};
+
+#endif // CANBUSCOMMUNICATIERPIBUS_H
