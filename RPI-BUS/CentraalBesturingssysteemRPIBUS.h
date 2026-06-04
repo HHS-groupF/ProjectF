c5cdec84b20f3e079484daaea371ec065de5fd27
Lucas Jansze <lucas.jansze@gmail.com>
2026-06-04 16:25:52 +0200

    Refactor CentraalBesturingssysteemRPIBUS class

--- RPI-BUS/CentraalBesturingssysteemRPIBUS.h
+++ RPI-BUS/CentraalBesturingssysteemRPIBUS.h
@@ -2,30 +2,39 @@
 #define CENTRAALBESTURINGSSYSTEEMRPIBUS_H
 
 #include <QObject>
-#include <QTimer>
 #include <QString>
 
 class CentraalBesturingssysteemRPIBUS : public QObject {
     Q_OBJECT
 public:
     explicit CentraalBesturingssysteemRPIBUS(QObject *parent = nullptr);
+
 public slots:
+    // Slot voor inkomende netwerkcommando's (bijv. vanaf een andere Pi of dashboard)
     void verwerkCommando(const QString &commando);
+
+    // Slot voor inkomende fysieke sensorwaardes vanaf de CAN-bus
+    void verwerkCanSensorData(uint32_t stmId, const QString &sensorType, double waarde);
+
 signals:
+    // Signaal om data via de TCP-socket te verzenden
     void stuurNetwerkData(const QString &data);
-private slots:
-    void genereerEnVerwerkSensorData();
+
+    // Signaal om een fysiek commando over de CAN-bus naar een STM32 te sturen
+    void stuurCanCommando(uint32_t stmId, const QByteArray &data);
+
 private:
-    QTimer *sensorTimer;
     double huidigTemp = 0.0;
     double huidigCO2 = 0.0;
     double huidigLucht = 0.0;
+
     bool isVentilatorAan = false;
     bool isBrandAlarmActief = false;
     bool isOverruleActief = false;
-    bool wasTeWarm = false;
-    bool wasTeVeelCO2 = false;
-    bool wasTeVochtig = false;
+
+    // Constante voor het CAN ID van de STM32 waar de ventilator (LED) fysiek aan gekoppeld zit
+    const uint32_t STM32_VENTILATOR_ID = 0x20;
+
     void controleerLimieten();
     void verzendSysteemStatus();
 };
