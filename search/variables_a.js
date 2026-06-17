5ebb02063e196a75ba7d2c30a4593a25d76b1aef
N64DudeTheRav <N64DudeTheRav@users.noreply.github.com>
2026-06-17 06:39:22 +0000

    deploy: 35333736e7591d559df07a596346af4114fd115b

--- search/variables_a.js
+++ search/variables_a.js
@@ -4,10 +4,11 @@ var searchData=
   ['rgb_5fblauw_1',['RGB_BLAUW',['../namespaceConfig.html#a858ba145ced3664dbd9054d370c57f32',1,'Config']]],
   ['rgb_5fgroen_2',['RGB_GROEN',['../namespaceConfig.html#a1476716c502944c1156777a0b527b4aa',1,'Config']]],
   ['rgb_5frood_3',['RGB_ROOD',['../namespaceConfig.html#a83082f29d1b82270051f1f2ea5c4a640',1,'Config']]],
-  ['rgb_5fwarm_4',['RGB_WARM',['../namespaceConfig.html#a8fad54eafe2187dc97bc9761566ce3be',1,'Config']]],
-  ['rgb_5fwit_5',['RGB_WIT',['../namespaceConfig.html#aa4ba2d2bd2ae3b711a59e1925271af62',1,'Config']]],
-  ['rpi_5fbus_5fip_6',['RPI_BUS_IP',['../namespaceConfig.html#a27ba7739239149b4862399632e93d469',1,'Config']]],
-  ['rpi_5fwemos_5fip_7',['RPI_WEMOS_IP',['../namespaceConfig.html#a278983ac47b9baab6695cc0a125b8ffd',1,'Config']]],
-  ['rxdata_8',['RxData',['../main_8c.html#a14629afcc7f0b2d62c2b617b07d98184',1,'main.c']]],
-  ['rxheader_9',['RxHeader',['../main_8c.html#a6ee6e13063b860648180eea65852cc71',1,'main.c']]]
+  ['rgb_5fuit_5fvertraging_4',['RGB_UIT_VERTRAGING',['../namespaceConfig.html#a60304f9518aa779b64d5e61d53acaa0f',1,'Config']]],
+  ['rgb_5fwarm_5',['RGB_WARM',['../namespaceConfig.html#a8fad54eafe2187dc97bc9761566ce3be',1,'Config']]],
+  ['rgb_5fwit_6',['RGB_WIT',['../namespaceConfig.html#aa4ba2d2bd2ae3b711a59e1925271af62',1,'Config']]],
+  ['rpi_5fbus_5fip_7',['RPI_BUS_IP',['../namespaceConfig.html#a27ba7739239149b4862399632e93d469',1,'Config']]],
+  ['rpi_5fwemos_5fip_8',['RPI_WEMOS_IP',['../namespaceConfig.html#a278983ac47b9baab6695cc0a125b8ffd',1,'Config']]],
+  ['rxdata_9',['RxData',['../main_8c.html#a14629afcc7f0b2d62c2b617b07d98184',1,'main.c']]],
+  ['rxheader_10',['RxHeader',['../main_8c.html#a6ee6e13063b860648180eea65852cc71',1,'main.c']]]
 ];
